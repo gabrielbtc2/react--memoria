73192d1d45b85c0266f64b82f09e325af4818055
Gabriel De Paula <gabrielcanalrobin@gmail.com>
2022-10-24 16:05:33 -0300

    segundo commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png';
+import RestartIcon from './svgs/restart.svg';
+import { InfoItem } from './components/InfoItem';
+import { Button } from './components/Button';
+
+const App = () => {
+
+  const resetAndCreateGrid = () => {
+
+  }
+
+  return (
+      <C.Container>
+          <C.Info>
+              <C.LogoLink href="">
+                  <img src={logoImage}  width="200" alt=""/>
+              </C.LogoLink>
+          
+              <C.InfoArea>
+                <InfoItem label="Tempo" value="00:00"/>
+                <InfoItem label="Movimentos" value="0"  />
+              </C.InfoArea>
+
+
+              <Button label="Reiniciar" icon={RestartIcon} onClick={resetAndCreateGrid} />
+              
+          </C.Info>
+          <C.GridArea>
+                ....
+          </C.GridArea>
+
+
+      </C.Container>
+  );
+}
+
+export default App;
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const IconArea = styled.div``;
+
+export const Icon = styled.img``;
+
+export const Label = styled.img``;
